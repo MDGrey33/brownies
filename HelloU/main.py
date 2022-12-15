c1116c498ea0a72a413e8638670f029910d7b9df
Roland Abou Younes <roland@abouyounes.com>
2022-12-15 23:37:05 +0200

    Website scraping - Images

--- HelloU/main.py
+++ HelloU/main.py
@@ -12,7 +12,17 @@ site_paragraphs = soup.select("p")
 
 res = requests.get('https://en.wikipedia.org/wiki/Grace_Hopper')
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-for item in soup.select('.toctext'):
-    print(item.text)
+# for item in soup.select('.toctext'):
+    # print(item.text)
 # print(soup.select('.toctext')[i])
 # print(text)
+
+res = requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+images = soup.select('.thumbimage')[0]
+print(images['src'])
+image_link = requests.get('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Kasparov_Magath_1985_Hamburg-2.png/220px-Kasparov_Magath_1985_Hamburg-2.png')
+print(image_link.content)
+f = open('my_computer_image.png', 'wb')
+f.write(image_link.content)
+f.close()
