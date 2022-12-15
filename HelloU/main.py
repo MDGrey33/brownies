7a485a1733a802df4a094fe774b4f8e1af2dba6b
Roland Abou Younes <roland@abouyounes.com>
2022-12-15 22:57:48 +0200

    Basic website scraping

--- HelloU/main.py
+++ HelloU/main.py
@@ -1,1 +1,18 @@
 # Web Scraping
+import requests, bs4, lxml
+
+result = requests.get('http://example.com/')
+# print(result.text)
+soup = bs4.BeautifulSoup(result.text, 'lxml')
+# element = soup.select('title')
+# element = soup.select('h1')
+element = soup.select('title')[0].get_text()
+site_paragraphs = soup.select("p")
+# print(site_paragraphs[0].getText())
+
+res = requests.get('https://en.wikipedia.org/wiki/Grace_Hopper')
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+for item in soup.select('.toctext'):
+    print(item.text)
+# print(soup.select('.toctext')[i])
+# print(text)
