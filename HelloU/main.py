5fdc39545c63bdecfa721949fd8a1a618b7b143a
Roland Abou Younes <roland@abouyounes.com>
2022-12-24 20:13:56 +0200

    Working with Emails

--- HelloU/main.py
+++ HelloU/main.py
@@ -1,1 +1,9 @@
-from PIL import Image
+# Working with emails
+import smtplib
+smtp_object = smtplib.SMTP('0.0.0.0', 1025)
+print(smtp_object.ehlo())
+
+from_address = 'from@senderdomain.com'
+to_address = 'to@receiver.com'
+message = 'Subject: this is a test email from pychatm \n hey this is the body of the email'
+smtp_object.sendmail(from_address, to_address, message)
